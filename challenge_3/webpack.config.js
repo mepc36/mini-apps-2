fe4c20d2e1538749e595e1afe9fcb1290131b8ac
Martin Connor <martinconnor@Martins-MacBook-Pro-3.local>
2019-06-27 11:58:41 -0700

    Configure webpack.

---
+++ challenge_3/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
